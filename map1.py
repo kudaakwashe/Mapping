caafbeff0323e1552731f8509fd38c8cbeb306f1
kudaakwashe <kudaakwashe@gmail.com>
2019-03-24 19:48:56 +0200

    Iterate through coordinates

--- map1.py
+++ map1.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location=[-33.92, 18.53], zoom_start=12, tiles="Stamen Toner")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[-33.91, 18.53], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+for coordinates in [[-33.91, 18.53],[-33.94, 18.57]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
 map.add_child(fg)
 
 map.save("Map1.html")
