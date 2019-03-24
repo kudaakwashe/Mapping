10aac45dc32b046b8ba0f8ff2120da8e56d3b634
kudaakwashe <kudaakwashe@gmail.com>
2019-03-24 20:18:07 +0200

    Use coordinates from a file

--- map1.py
+++ map1.py
@@ -1,10 +1,16 @@
 import folium
-map = folium.Map(location=[-33.92, 18.53], zoom_start=12, tiles="Stamen Toner")
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=12, tiles="Stamen Toner")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[-33.91, 18.53],[-33.94, 18.57]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
